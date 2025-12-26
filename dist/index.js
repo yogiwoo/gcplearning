b5bcba90a1633a3a7abc71dc75111a750b9d770f
yogesh kumar <yogiwoo@gmail.com>
2025-12-26 14:10:06 +0530

    port changing for cloud run

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.port || 8080;
 app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
